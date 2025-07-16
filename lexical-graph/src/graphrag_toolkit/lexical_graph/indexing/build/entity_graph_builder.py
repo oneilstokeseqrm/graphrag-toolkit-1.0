ecee476c2ea2c630023d15c5462e2bcec21647bb
ianrob <ianrob@amazon.co.uk>
2025-07-16 11:00:10 +0100

    batch up parameterless queries and run after other queries

--- lexical-graph/src/graphrag_toolkit/lexical_graph/indexing/build/entity_graph_builder.py
+++ lexical-graph/src/graphrag_toolkit/lexical_graph/indexing/build/entity_graph_builder.py
@@ -6,7 +6,7 @@ from typing import Any
 
 from graphrag_toolkit.lexical_graph.indexing.model import Fact
 from graphrag_toolkit.lexical_graph.storage.graph import GraphStore
-from graphrag_toolkit.lexical_graph.storage.graph.graph_utils import search_string_from, label_from
+from graphrag_toolkit.lexical_graph.storage.graph.graph_utils import search_string_from, label_from, new_query_var
 from graphrag_toolkit.lexical_graph.indexing.build.graph_builder import GraphBuilder
 from graphrag_toolkit.lexical_graph.indexing.constants import DEFAULT_CLASSIFICATION
 
@@ -111,45 +111,22 @@ class EntityGraphBuilder(GraphBuilder):
             graph_client.execute_query_with_retry(query, self._to_params(properties), max_attempts=5, max_wait=7)
 
             if include_domain_labels:
-            
-                statements_s = [
-                    '// add domain-specific labels',
-                    'UNWIND $params AS params'
-                ]
 
-                statements_s.extend([
-                    f'MERGE (n:`__Entity__`{{{graph_client.node_id("entityId")}: params.n_id}})',
-                    f'SET n :{label_from(fact.subject.classification or DEFAULT_CLASSIFICATION)}', 
-                ])
-
-                properties_s = {
-                    'n_id': fact.subject.entityId
-                }
-
-                query_s = '\n'.join(statements)
-                
-                graph_client.execute_query_with_retry(query_s, self._to_params(properties_s), max_attempts=5, max_wait=7)
+                s_var = new_query_var()
+                s_id = fact.subject.entityId
+                s_label = label_from(fact.subject.classification or DEFAULT_CLASSIFICATION)
+                s_comment = f'// awsqid:{s_id}-{s_label}'
+                query_s = f"MERGE ({s_var}:`__Entity__`{{{graph_client.node_id('entityId')}: '{s_id}'}}) SET {s_var} :`{s_label}` {s_comment}"    
+                graph_client.execute_query_with_retry(query_s, {}, max_attempts=5, max_wait=7)
 
                 if fact.object and fact.object.entityId != fact.subject.entityId:
 
-                    statements_o = [
-                        '// add domain-specific labels',
-                        'UNWIND $params AS params'
-                    ]
-
-                    statements_o.extend([
-                        f'MERGE (n:`__Entity__`{{{graph_client.node_id("entityId")}: params.n_id}})',
-                        f'SET n :{label_from(fact.object.classification or DEFAULT_CLASSIFICATION)}', 
-                    ])
-
-                    properties_o = {
-                        'n_id': fact.object.entityId
-                    }
-
-                    query_o = '\n'.join(statements)
-                    
-                    graph_client.execute_query_with_retry(query_o, self._to_params(properties_o), max_attempts=5, max_wait=7)
-           
+                    o_var = new_query_var()
+                    o_id = fact.object.entityId
+                    o_label = label_from(fact.object.classification or DEFAULT_CLASSIFICATION)
+                    o_comment = f'// awsqid:{o_id}-{o_label}'
+                    query_o = f"MERGE ({o_var}:`__Entity__`{{{graph_client.node_id('entityId')}: '{o_id}'}}) SET {o_var} :`{o_label}` {o_comment}"  
+                    graph_client.execute_query_with_retry(query_o, {}, max_attempts=5, max_wait=7)
 
         else:
             logger.warning(f'fact_id missing from fact node [node_id: {node.node_id}]')
